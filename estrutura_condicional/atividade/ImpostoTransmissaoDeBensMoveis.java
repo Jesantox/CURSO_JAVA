5737a8c8ddeac4791d2d60d8f69b43c89dd547ad
Jesantox <Jesantoxfachmann@gmail.com>
2021-03-22 03:19:25 -0300

    Estruturas Condicionais

---
+++ estrutura_condicional/atividade/ImpostoTransmissaoDeBensMoveis.java
@@ -0,0 +1,53 @@
+package estrutura_condicional.atividade;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ImpostoTransmissaoDeBensMoveis {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(" Olá, tudo bem? Como se chama?");
+		String nome  = sc.nextLine();
+		
+		System.out.println(" Prazer, " + nome + ".");
+		
+		System.out.println(" Insira o valor da Transação : ");
+		double transacao= sc.nextDouble();
+		
+		System.out.println(" Insira o custo do valorVenal : ");
+		double valorVenal = sc.nextDouble();
+		
+		int porcentagemValor = 0;
+		double porcentagemCalculo = 0;
+		double valorFinal = 0;
+		String simbolo = "%";
+		
+		if(transacao > valorVenal) {
+			System.out.println(" Informe o Percentual : ");
+			porcentagemValor = sc.nextInt();
+			porcentagemCalculo = (transacao * porcentagemValor) / 100;
+			valorFinal = transacao - porcentagemCalculo;
+		}
+		
+		else {
+			System.out.println(" Informe o Percentual : ");
+			porcentagemValor = sc.nextInt();
+			porcentagemCalculo = (valorVenal * porcentagemValor) / 100;
+			valorFinal = valorVenal - porcentagemCalculo;
+		}
+		
+		System.out.printf(" %n Olá, %s. "
+				        + " %n Segue abaixo as análises. "
+				        + " %n Valor Percentual : %d%s " 
+				        + " %n Valor do Desconto : R$ %.2f" 
+				        + " %n Imposto Final : R$ %.2f", nome, porcentagemValor, simbolo, porcentagemCalculo, valorFinal);
+		
+		sc.close();
+
+	}
+
+}
---
+++ estrutura_condicional/atividade/MonitorarFaixaEtaria.java
@@ -0,0 +1,39 @@
+package estrutura_condicional.atividade;
+
+import java.util.Scanner;
+
+public class MonitorarFaixaEtaria {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println(" Olá, tudo bem? Como se chama? ");
+		String nome = sc.nextLine();
+
+		System.out.println(" Olá, " + nome + ". \n Você tem quantos anos?");
+		int idade = sc.nextInt();
+
+		if (idade >= 0 && idade <= 3)
+			System.out.printf("%n Olá, %s. %n Você tem %d anos. Ou seja, um Bebê. ", nome, idade);
+
+		else if (idade > 3 && idade <= 12)
+			System.out.printf("%n Olá, %s. %n Você tem %d anos. Ou seja, uma Criança. ", nome, idade);
+
+		else if (idade > 12 && idade <= 18)
+			System.out.printf("%n Olá, %s. %n Você tem %d anos. Ou seja, um Adolescente. ", nome, idade);
+
+		else if (idade > 18 && idade <= 70)
+			System.out.printf("%n Olá, %s. %n Você tem %d anos. Ou seja, um Adulto. ", nome, idade);
+
+		else if (idade > 70 && idade <= 120)
+			System.out.printf("%n Olá, %s. %n Você tem %d anos. Ou seja, está na Terceira Idade. ", nome, idade);
+
+		else if (idade > 120)
+			System.out.printf("%n Olá, %s. %n Você tem %d anos. Parabens por viver tanto :O ", nome, idade);
+
+		sc.close();
+
+	}
+
+}
