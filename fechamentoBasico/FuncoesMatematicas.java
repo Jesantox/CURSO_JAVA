cb6eaa0ed0d63b8a76fe7b0cc45357c82b0bcad6
Jesantox <Jesantoxfachmann@gmail.com>
2021-05-02 02:19:52 -0300

    Função - Operações Básicas de Matemática

---
+++ fechamentoBasico/FuncoesMatematicas.java
@@ -0,0 +1,72 @@
+package fechamentoBasico;
+
+/**
+  @author <b> Jesantox </b>
+  
+  <h1> Funções Matemáticas - <i> Operações Básicas </i> </h1>
+  
+  <b> Adição        </b>
+  <b> Subtração     </b>
+  <b> Divisão       </b>
+  <b> Multiplicação </b>
+ 
+ */
+public class FuncoesMatematicas {
+
+	/**
+	 
+	 <b> Soma Valores </b>
+	 
+	 @param a
+	 @param b
+	 @return --> <i> Retorna o resultado da Soma </i>
+	 */
+	public static int adicao(int a, int b) {
+		int adicao = a + b;
+		
+		return adicao;
+	}
+	
+	/**
+	 
+	 <b> Subtrai Valores </b>
+	 
+	 @param a
+	 @param b
+	 @return --> <i> Retorna o resultado da Subtração </i>
+	 */
+	public static int subtracao(int a, int b) {
+		int subtracao = a - b;
+		
+		return subtracao;
+	}
+	
+	/**
+	 
+	 <b> Dividi Valores </b>
+	 
+	 @param a
+	 @param b
+	 @return --> <i> Retorna o resultado da Divisão </i>
+	 */
+	public static int divisao(int a, int b) {
+		int divisao = a / b;
+		
+		return divisao;
+	}
+	
+	/**
+	
+	 <u> <b> Multiplica Valores </b> </u>
+	 
+	 @param a
+	 @param b
+	 @return -->  <i> Retorna o resultado da Multiplicação </i> 
+	 */
+	public static int multiplicacao(int a, int b) {
+		int multiplicacao = a * b;
+		
+		return multiplicacao;
+	}
+
+}
